d465abaeff2ff6096594eefc9c11381bac2dfa98
DNKpp <dnkpp2011@gmail.com>
2025-12-07 14:23:09 +0100

    chore: replace std::expected based and_then test with testing::ExpectedFake

--- test/algorithm/AndThen.cpp
+++ test/algorithm/AndThen.cpp
@@ -4,7 +4,6 @@
 //           https://www.boost.org/LICENSE_1_0.txt)
 
 #include "gimo/algorithm/AndThen.hpp"
-#include "gimo_ext/std_expected.hpp"
 #include "gimo_ext/std_optional.hpp"
 
 #include "TestCommons.hpp"
@@ -171,35 +170,35 @@ TEMPLATE_LIST_TEST_CASE(
     using with_qualification = TestType;
 
     mimicpp::Mock<
-        std::expected<int, std::string>(float)&,
-        std::expected<int, std::string>(float) const&,
-        std::expected<int, std::string>(float)&&,
-        std::expected<int, std::string>(float) const&&>
+        testing::ExpectedFake<int>(float)&,
+        testing::ExpectedFake<int>(float) const&,
+        testing::ExpectedFake<int>(float)&&,
+        testing::ExpectedFake<int>(float) const&&>
         action{};
 
     using Algorithm = detail::and_then_t<decltype(action)>;
-    STATIC_REQUIRE(gimo::applicable_on<std::expected<float, int>, typename with_qualification::template type<Algorithm>>);
+    STATIC_REQUIRE(gimo::applicable_on<testing::ExpectedFake<float>, typename with_qualification::template type<Algorithm>>);
 
     SECTION("When input has a value, the action is invoked.")
     {
-        constexpr std::expected<float, std::string> expected{1337.f};
+        constexpr testing::ExpectedFake expected{1337.f};
 
         SCOPED_EXP with_qualification::cast(action).expect_call(1337.f)
             and finally::returns(42);
 
         Algorithm andThen{std::move(action)};
         decltype(auto) result = std::invoke(with_qualification::cast(andThen), expected);
-        STATIC_REQUIRE(std::same_as<std::expected<int, std::string>, decltype(result)>);
-        CHECK(42 == result);
+        STATIC_REQUIRE(std::same_as<testing::ExpectedFake<int>, decltype(result)>);
+        CHECK(42 == *result);
     }
 
     SECTION("When input holds an error, the error is forwarded.")
     {
-        std::expected<float, std::string> const expected{std::unexpected{"An error."}};
+        auto const expected = testing::ExpectedFake<float>::from_error("An error.");
         Algorithm andThen{std::move(action)};
 
         decltype(auto) result = std::invoke(with_qualification::cast(andThen), expected);
-        STATIC_REQUIRE(std::same_as<std::expected<int, std::string>, decltype(result)>);
+        STATIC_REQUIRE(std::same_as<testing::ExpectedFake<int>, decltype(result)>);
         CHECK(!result);
         CHECK_THAT(
             result.error(),
