7e498be3c06c622d4bc877b81f17011f8e677df6
DNKpp <dnkpp2011@gmail.com>
2025-12-11 19:18:38 +0100

    refactor: remove gimo include from std_optional.hpp

--- include/gimo_ext/std_optional.hpp
+++ include/gimo_ext/std_optional.hpp
@@ -1,16 +1,20 @@
-//           Copyright Dominic (DNKpp) Koepke 2025.
-//  Distributed under the Boost Software License, Version 1.0.
-//     (See accompanying file LICENSE_1_0.txt or copy at
-//           https://www.boost.org/LICENSE_1_0.txt)
+//          Copyright Dominic (DNKpp) Koepke 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef GIMO_EXT_STD_OPTIONAL_HPP
 #define GIMO_EXT_STD_OPTIONAL_HPP
 
 #pragma once
 
-#include "gimo/Common.hpp"
-
 #include <optional>
+
+namespace gimo
+{
+    template <typename T>
+    struct traits;
+}
 
 template <typename T>
 struct gimo::traits<std::optional<T>>
