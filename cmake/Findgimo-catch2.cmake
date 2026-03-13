b0e8f054bf6d7f6524cb586868f0dd07ef176d17
DNKpp <dnkpp2011@gmail.com>
2026-03-13 17:06:17 +0100

    deps: upgrade catch2 to v3.13.0

--- cmake/Findgimo-catch2.cmake
+++ cmake/Findgimo-catch2.cmake
@@ -1,4 +1,4 @@
-#          Copyright Dominic (DNKpp) Koepke 2025 - 2025.
+#          Copyright Dominic (DNKpp) Koepke 2025 - 2026.
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          https://www.boost.org/LICENSE_1_0.txt)
@@ -8,7 +8,7 @@ include(Gimo-get_cpm)
 # fixes reporting in clion
 set(CATCH_CONFIG_CONSOLE_WIDTH 800 CACHE STRING "")
 
-CPMAddPackage("gh:catchorg/Catch2@3.11.0")
+CPMAddPackage("gh:catchorg/Catch2@3.13.0")
 
 if (Catch2_ADDED)
 	include("${Catch2_SOURCE_DIR}/extras/Catch.cmake")
