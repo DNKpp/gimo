9df0a044044d4c322fb8af235c109c46763805b2
DNKpp <DNKpp@users.noreply.github.com>
2026-05-30 08:18:03 +0000

    deploy: 80f34a434c046b7305262e2c6aa9c8c864629a07

--- concepts.js
+++ concepts.js
@@ -2,14 +2,14 @@ var concepts =
 [
     [ "gimo", "d6/d90/namespacegimo.html", [
       [ "applicable_to", "d4/d8f/conceptgimo_1_1applicable__to.html", null ],
+      [ "constructible_from_error", "d3/df0/conceptgimo_1_1constructible__from__error.html", null ],
+      [ "constructible_from_value", "da/d82/conceptgimo_1_1constructible__from__value.html", null ],
+      [ "expected_like", "dc/dcd/conceptgimo_1_1expected__like.html", null ],
       [ "null_for", "d8/db7/conceptgimo_1_1null__for.html", null ],
       [ "nullable", "d8/d8e/conceptgimo_1_1nullable.html", null ],
-      [ "constructible_from_value", "da/d82/conceptgimo_1_1constructible__from__value.html", null ],
-      [ "rebindable_value_to", "df/dbc/conceptgimo_1_1rebindable__value__to.html", null ],
-      [ "expected_like", "dc/dcd/conceptgimo_1_1expected__like.html", null ],
-      [ "constructible_from_error", "d3/df0/conceptgimo_1_1constructible__from__error.html", null ],
-      [ "rebindable_error_to", "da/d07/conceptgimo_1_1rebindable__error__to.html", null ],
       [ "pipeline", "de/d0a/conceptgimo_1_1pipeline.html", null ],
-      [ "processable_by", "d1/d5d/conceptgimo_1_1processable__by.html", null ]
+      [ "processable_by", "d1/d5d/conceptgimo_1_1processable__by.html", null ],
+      [ "rebindable_error_to", "da/d07/conceptgimo_1_1rebindable__error__to.html", null ],
+      [ "rebindable_value_to", "df/dbc/conceptgimo_1_1rebindable__value__to.html", null ]
     ] ]
 ];
